ffffbd88e21d55b9fa3de017f48c777aba16dff1
Jamie Brandon <jamie@scattered-thoughts.net>
2022-10-16 13:47:19 -0700

    mystery1.zig

--- mystery1.zig
+++ mystery1.zig
@@ -6,14 +6,18 @@ const k_iterations = 1_000 * 1_000_000;
 pub fn main() void {
     var sum: u64 = 0;
 
+    const time = std.time.timestamp();
+    const incr = @intCast(u64, time) & 255;
+
     const start = util.rdtscp();
     var i: usize = 0;
     while (i < k_iterations) : (i += 1) {
-        sum += 1;
+        sum += incr;
     }
     const elapsed = util.rdtscp() - start;
 
-    std.debug.print("{} iterations, {} cycles, {d:0>6.2} cycles/iteration", .{
+    std.debug.print("{} sum, {} iterations, {} cycles, {d:0>6.2} cycles/iteration", .{
+        sum,
         k_iterations,
         elapsed,
         @intToFloat(f64, elapsed) / @intToFloat(f64, k_iterations),
