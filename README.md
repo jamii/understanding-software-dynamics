5d99240939a7ebdee29e750dfa46293b0693c68c
Jamie Brandon <jamie@scattered-thoughts.net>
2022-10-19 15:11:10 -0700

    Really fixed

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 https://www.goodreads.com/en/book/show/57850403-understanding-software-dynamics
+
+## Chapter 2
+
+```
+> zig build-exe mystery1-fixed.zig -OReleaseFast && sudo benchmark ./mystery1-fixed
+cset: --> last message, executed args into cpuset "/user", new pid is: 26911
+64x unrolled, 16 iterations
+.add 	   1253 cycles 	   1.22 cycles/iteration 	 total=3075
+.mul 	   4914 cycles 	   4.80 cycles/iteration 	 total=2959831043
+.imul 	   3691 cycles 	   3.60 cycles/iteration 	 total=2959831043
+.div 	  14562 cycles 	  14.22 cycles/iteration 	 total=0
+.idiv 	  14566 cycles 	  14.22 cycles/iteration 	 total=0
+.fmul 	   5044 cycles 	   4.93 cycles/iteration 	 total=7.98369218e+04
+.fdiv 	  19751 cycles 	  19.29 cycles/iteration 	 total=3.15215270e+02
+```
+
+For alder lake https://www.uops.info/table.html gives:
+
+* add => 1
+* mul/imul => 3:4
+* div/idiv => 10:15
+* fmul => no data (5 for haswell)
+* fdiv => no data (15 for amd zen)
+
+If we crank the iterations to 1<<20:
+
+```
+> zig build-exe mystery1-fixed.zig -OReleaseFast && sudo benchmark ./mystery1-fixed
+cset: --> last message, executed args into cpuset "/user", new pid is: 26953
+64x unrolled, 16384 iterations
+.add 	 1249209 cycles 	   1.19 cycles/iteration 	 total=3145731
+.mul 	 5015783 cycles 	   4.78 cycles/iteration 	 total=2915041283
+.imul 	 3758952 cycles 	   3.58 cycles/iteration 	 total=2915041283
+.div 	 14921922 cycles 	  14.23 cycles/iteration 	 total=0
+.idiv 	 15039220 cycles 	  14.34 cycles/iteration 	 total=0
+.fmul 	 477845056 cycles 	 455.71 cycles/iteration 	 total=inf
+.fdiv 	 33096288 cycles 	  31.56 cycles/iteration 	 total=1.40129846e-45
+```
+
+Floating point ops seem slower for outside normal range.
--- mystery1-fixed.zig
+++ mystery1-fixed.zig
@@ -1,31 +1,111 @@
 const std = @import("std");
 const util = @import("util.zig");
 
-const k_iterations = 1_000 * 1_000_000;
+const total_count = 1 << 10;
+const unroll_count = 1 << 6;
+const loop_count_max = @divExact(total_count, unroll_count);
 
-const unrolled_iterations = 10;
+pub fn run(comptime op: anytype) void {
+    var total = switch (op) {
+        .add, .mul, .imul => @as(u32, 3),
+        .div, .idiv => @as(u32, std.math.maxInt(u32)),
+        .fmul => @as(f32, 3),
+        .fdiv => @intToFloat(f32, std.math.maxInt(u23)),
+        else => unreachable,
+    };
 
-pub fn main() void {
-    var sum: u32 = 0;
-
-    const time = std.time.timestamp();
-    const incr = @intCast(u64, time) & 255;
+    const incr = switch (op) {
+        .add, .mul, .imul, .div, .idiv => @as(u32, 3),
+        .fmul, .fdiv => @as(f32, 1.01),
+        else => unreachable,
+    };
 
     const start = util.rdtscp();
-    var i: usize = 0;
-    while (i < k_iterations) : (i += 1) {
-        asm volatile ("add %%ebx, %%eax;\n" ** unrolled_iterations
-            : [sum] "={eax}" (sum),
-            : [sum] "{eax}" (sum),
-              [incr] "{ebx}" (incr),
-        );
+
+    var loop_index: usize = 0;
+    while (loop_index < loop_count_max) : (loop_index += 1) {
+        switch (op) {
+            .add => asm volatile (
+                \\add %%ebx, %%eax;
+                ** unroll_count
+                : [total] "={eax}" (total),
+                : [total] "{eax}" (total),
+                  [incr] "{ebx}" (incr),
+            ),
+            .mul => asm volatile (
+                \\mul %%ebx;
+                ** unroll_count
+                : [total] "={eax}" (total),
+                : [total] "{eax}" (total),
+                  [incr] "{ebx}" (incr),
+                : "edx"
+            ),
+            .imul => asm volatile (
+                \\imul %%ebx, %%eax;
+                ** unroll_count
+                : [total] "={eax}" (total),
+                : [total] "{eax}" (total),
+                  [incr] "{ebx}" (incr),
+            ),
+            .div => asm volatile (
+                \\xor %%edx, %%edx;
+                \\div %%ebx;
+                ** unroll_count
+                : [total] "={eax}" (total),
+                : [total] "{eax}" (total),
+                  [incr] "{ebx}" (incr),
+                : "edx"
+            ),
+            .idiv => asm volatile (
+                \\xor %%edx, %%edx;
+                \\idiv %%ebx;
+                ** unroll_count
+                : [total] "={eax}" (total),
+                : [total] "{eax}" (total),
+                  [incr] "{ebx}" (incr),
+                : "edx"
+            ),
+            .fmul => asm volatile (
+                \\fmul %%st(1), %%st(0);
+                ** unroll_count
+                : [total] "={st(0)}" (total),
+                : [total] "{st(0)}" (total),
+                  [incr] "{st(1)}" (incr),
+            ),
+            .fdiv => asm volatile (
+                \\fdiv %%st(1), %%st(0);
+                ** unroll_count
+                : [total] "={st(0)}" (total),
+                : [total] "{st(0)}" (total),
+                  [incr] "{st(1)}" (incr),
+            ),
+            else => unreachable,
+        }
     }
+
     const elapsed = util.rdtscp() - start;
 
-    std.debug.print("{} sum, {} iterations, {} cycles, {d:>6.2} cycles/iteration", .{
-        sum,
-        k_iterations,
+    std.debug.print("{:>4} \t {d:>6.0} cycles \t {d:>6.2} cycles/iteration \t total={}\n", .{
+        op,
         elapsed,
-        @intToFloat(f64, elapsed) / unrolled_iterations / @intToFloat(f64, k_iterations),
+        @intToFloat(f64, elapsed) / loop_count_max / unroll_count,
+        total,
     });
+}
+
+pub fn main() void {
+    std.debug.print("{}x unrolled, {} iterations\n", .{
+        unroll_count,
+        loop_count_max,
+    });
+
+    inline for (.{
+        .add,
+        .mul,
+        .imul,
+        .div,
+        .idiv,
+        .fmul,
+        .fdiv,
+    }) |op| run(op);
 }
